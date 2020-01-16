1bc0e727b7188eb898315df555d13f11f70881b5
Ben Noah <noahbenaas@gmail.com>
2020-01-16 21:49:36 +0100

    missing 9 and 10

--- script.js
+++ script.js
@@ -13,3 +13,73 @@ console.log(petArray [1]); // second step
 
 // 3: Add the item "sheep" to petArray using the push method.
 
+console.log(petArray.push("sheep"));
+
+// 4. Console log how many items there are now in petArray using the length method.
+
+console.log(petArray.length);//4
+
+// 5.Create an object called catObject with 3 properties: name, colour, age.
+// Add appropriate values (and type of values) for each of these properties.
+var catObject={
+    name:"kris",
+    colour:"green",
+    age:16
+};
+
+// 6. Console log the value of the third property of catObject.
+
+catObject.age;
+
+// 7.Take the value of catObject (the {} part) and place it inside an array called catArray. Ask Teacher
+              
+    catArray = ["kris", "green", 16];
+
+
+// 8.Loop throughcatArray and console log each property of each object inside (there will be only 1 object inside).
+
+for (var i=0; i<catArray.length; i++){
+    console.log(catArray[i]);
+};
+
+// 9. Create a function called logToConsole. 
+// The function should accept 1 argument. 
+//Decide on the name of the argument and console log its value inside the function.
+
+function logToConsole(happy) {
+    console.log(happy);
+}
+logToConsole("hello");
+VM1828:2 hello
+
+// 10.call the logToConsole
+
+
+
+
+/*
+var posts = [
+    {
+        imageUrl: "https://path/to/bee-picture",
+        likeCounter: 80,
+        likedByUser: true
+    },
+    {
+        imageUrl: "https://path/to/bee-picture",
+        likeCounter: 80,
+        likedByUser: true
+    },
+    {
+        imageUrl: "https://path/to/bee-picture",
+        likeCounter: 80,
+        likedByUser: true
+    }
+];
+
+
+for (var i= 0; i< posts.length; i++){
+    console.log(posts[i].imageUrl);
+};
+
+
+*/
