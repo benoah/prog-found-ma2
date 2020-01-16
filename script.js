2afccd93fbcbfcec7e04ad71c4a26d6fd54839f9
Ben Noah <noahbenaas@gmail.com>
2020-01-16 12:15:15 +0100

    question 3

---
+++ script.js
@@ -0,0 +1,15 @@
+// 1.Create an array called petArray with 3 string items in it: "cat", "cow", "dog".
+
+var petArray = ["cat", "cow", "dog"];
+
+// 2.Console log the value of the second item in petArray.
+
+petArray.length //FIRST STEP //ANSWER 3
+
+console.log(petArray [1]); // second step 
+
+//answer cow
+
+
+// 3: Add the item "sheep" to petArray using the push method.
+
